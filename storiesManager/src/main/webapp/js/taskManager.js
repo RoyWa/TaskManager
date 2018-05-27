16ae277461bde068e4482d4a80c66c28227e8cc1
roy.wallerstein <roy.wallerstein@servicenow.com>
2018-05-27 08:17:18 +0300

    Fix UI card flipping

--- storiesManager/src/main/webapp/js/taskManager.js
+++ storiesManager/src/main/webapp/js/taskManager.js
@@ -1,22 +1,38 @@
 
-function flip(obj){
-  TweenLite.to($(this).find(".card"), 1.2, {rotationY:180, ease:Back.easeOut})
-}
 
 class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isCardFaceFront:true
+        };
+    }
+
+    componentDidUpdate(){
+        TweenLite.set('.cardWrapper', {perspective: 800});
+        TweenLite.set('.card', {transformStyle: 'preserve-3d'});
+        TweenLite.set('.back', {rotationY: -180});
+        TweenLite.set(['.back', '.front'], {backfaceVisibility: 'hidden'});
+        //var $this = $(ReactDOM.findDOMNode(this));
+    }
+    
+    handleFlipClick(objId){
+        const card = $("div[data-id='"+objId+"']");
+        const rotationDegree = (this.state.isCardFaceFront) ? 180 : 0;
+        TweenLite.to(card, 1.2, {rotationY:rotationDegree, ease:Back.easeOut});
+        this.setState({
+            isCardFaceFront: !this.state.isCardFaceFront
+        });
+    }
+
     renderBoard() {
         if ( this.props.stories.map) {
-            TweenLite.set('.cardWrapper', {perspective: 800});
-            TweenLite.set('.card', {transformStyle: 'preserve-3d'});
-            TweenLite.set('.back', {rotationY: -180});
-            TweenLite.set(['.back', '.front'], {backfaceVisibility: 'hidden'});
-
             const stories = this.props.stories.map((story, step) => {
                 const num = story.number;
                 const desc = story.short_description;
                 return (  <div key={step}>
-                            <div className="cardWrapper answer-animation"  onClick={() => flip(this) } >
-                                <div className="card" >
+                            <div className="cardWrapper answer-animation"  >
+                                <div className="card" data-id={"card_"+ step} onClick={() => this.handleFlipClick("card_"+ step) } >
                                     <div className="cardFace front" ><h1>(-: </h1></div>
                                     <div className="cardFace back"  ><h1>{num} - {desc}</h1></div>
                                 </div>
@@ -26,7 +42,7 @@ class Board extends React.Component {
             });
             return ( <div>{stories}</div>);
         } 
-        return 'bbb';
+        return '';
     }
     
     render() {
@@ -55,5 +71,5 @@ class TaskManager extends React.Component {
 
 ReactDOM.render(
     <TaskManager />,
-    document.getElementById('root')
+    document.getElementById('root'),
 );
